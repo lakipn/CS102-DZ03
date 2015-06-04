76d9d64aec723c968a5475d42f10326833ef748d
Lazar <Lazar@Lazar-Dell>
2015-06-04 19:23:04 +0200

    Zakasneli commit, svašta dodato...

--- src/cs102dz03/CS102DZ03.java
+++ src/cs102dz03/CS102DZ03.java
@@ -5,18 +5,22 @@
  */
 package cs102dz03;
 
+import java.awt.event.ActionListener;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javax.swing.Timer;
 
 /**
  *
@@ -25,12 +29,14 @@ import javafx.stage.Stage;
 public class CS102DZ03 extends Application {
     
     private Pane root;
-    private Label lblBrojPitanja, lblOdgovor, lblStatistika;
+    private Label lblBrojPitanja, lblOdgovor, lblStatistika, lblPercentage;
     private TextArea txtPitanje;
     private TextField txtOdgovor;
     private Button btnSubmit, btnExit;
+    private ProgressBar pBar;
     
     private Logika mozak;
+    Timer timer;
     
     @Override
     public void start(Stage primaryStage) {
@@ -41,6 +47,8 @@ public class CS102DZ03 extends Application {
         txtOdgovor = new TextField("");
         btnSubmit = new Button("Potvrdi");
         btnExit = new Button("Izađi");
+        pBar = new ProgressBar();
+        lblPercentage = new Label("Preostalo vreme: 9.99 sekundi");
                       
         root = new Pane();
         
@@ -48,11 +56,57 @@ public class CS102DZ03 extends Application {
         
         setElementsOnPane();
         
-        Scene scene = new Scene(root, 700, 200);
+        mozak.setTxtPitanje(txtPitanje);
+        mozak.setTxtOdgovor(txtOdgovor);
+        mozak.setLblStatistika(lblStatistika);
+        mozak.setProgressBar(pBar);
+        mozak.setLblPercentage(lblPercentage);
+        
+        Scene scene = new Scene(root, 700, 240);
         primaryStage.setTitle("Ko zna - zna");
         primaryStage.setScene(scene);
         primaryStage.show();
         
+        timer = new Timer(1000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                
+                Platform.runLater(() ->
+                {   
+                    mozak.protekloVreme++;
+                    if(mozak.protekloVreme == 10)
+                    {
+                        mozak.handleProsloVremeIliDatOdgovor();
+                        txtPitanje.setText(mozak.getTrenutnoPitanje().getPitanje());
+                        lblBrojPitanja.setText("Redni broj pitanja: " + (mozak.getBilaPitanjaSize()));
+                    }
+                });
+                
+            }
+        });
+        
+        //mozak.startTimer();
+        mozak.handleProsloVremeIliDatOdgovor();
+        txtPitanje.setText(mozak.getTrenutnoPitanje().getPitanje());
+        lblBrojPitanja.setText("Redni broj pitanja: " + (mozak.getBilaPitanjaSize()));
+        timer.start();
+        
+        ProgressBarThread pBarThrd = new ProgressBarThread("ProgressBar nit", pBar, mozak);
+        pBarThrd.start();
+        
+        Timer ugradniTimer = new Timer(100, new ActionListener() {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                
+                Platform.runLater(() -> {
+                    lblPercentage.setText("Preostalo vreme: " + (9 - mozak.protekloVreme) + " sekundi");
+                });
+                
+            }
+        });
+        ugradniTimer.start();
     }
 
     /**
@@ -89,6 +143,16 @@ public class CS102DZ03 extends Application {
         btnSubmit.setPrefWidth(100);
         btnSubmit.setPrefHeight(30);
         btnSubmit.setFont(Font.font("Arial", 20));
+        
+        btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                mozak.handleProsloVremeIliDatOdgovor();
+                txtPitanje.setText(mozak.getTrenutnoPitanje().getPitanje());
+                lblBrojPitanja.setText("Redni broj pitanja: " + mozak.getBilaPitanjaSize());
+            }
+        });
         
         root.getChildren().add(btnSubmit);
     }
@@ -143,6 +207,23 @@ public class CS102DZ03 extends Application {
         root.getChildren().add(lblStatistika);
     }
     
+    private void setPBarOnPane()
+    {
+        pBar.setLayoutX(25);
+        pBar.setLayoutY(200);
+        pBar.setPrefWidth(650);
+        pBar.setPrefHeight(25);
+        pBar.setProgress(0);
+        pBar.setVisible(true);
+        
+        lblPercentage.setLayoutX(250);
+        lblPercentage.setLayoutY(205);
+        lblPercentage.setPrefWidth(170);
+        
+        root.getChildren().add(pBar);
+        root.getChildren().add(lblPercentage);
+    }
+    
     private void setElementsOnPane()
     {
         setLblBrojPitanjaOnPane();
@@ -152,5 +233,6 @@ public class CS102DZ03 extends Application {
         setTxtOdgovorOnPane();
         setBtnSubmitOnPane();
         setBtnExitOnPane();
+        setPBarOnPane();
     }
 }
